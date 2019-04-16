d02a0ea721466b76fed80c96552242755fa17354
fucsky <49684329+fucsky@users.noreply.github.com>
2019-04-16 21:24:17 +0800

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,26 +1,14 @@
 <?php
+namespace think;
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  // $app['monolog']->addDebug('logging output.');
-  // return $app['twig']->render('index.twig');
-  return file_get_contents('http://icanhazip.com');
-});
-
-$app->run();
+if(version_compare(PHP_VERSION,'5.6.0','<'))  die('PHP版本过低，最少需要PHP5.6，请升级PHP版本！');
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/application/');
+// 加载框架基础引导文件
+require __DIR__ . '/thinkphp/base.php';
+// 执行应用并响应
+if(!is_file(APP_PATH . 'install/data/install.lock')){
+	Container::get('app')->path(APP_PATH)->bind('install')->run()->send();
+}else{
+	Container::get('app')->path(APP_PATH)->run()->send();
+}
